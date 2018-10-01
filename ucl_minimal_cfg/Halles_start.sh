4c82adfdde7471519df753fac7ef94060a7d1369
Vagrant Default User <vagrant@jessie.vagrantup.com>
2018-10-01 15:10:43 +0000

    Add ip on Pyth and Halles

--- ucl_minimal_cfg/Halles_start.sh
+++ ucl_minimal_cfg/Halles_start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 puppet apply --verbose --parser future --hiera_config=/etc/puppet/hiera.yaml /etc/puppet/site.pp --modulepath=/puppetmodules
+ip addr add fd00:300::4/48 dev Halles-eth1
--- ucl_minimal_cfg/Pythagore_start.sh
+++ ucl_minimal_cfg/Pythagore_start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 puppet apply --verbose --parser future --hiera_config=/etc/puppet/hiera.yaml /etc/puppet/site.pp --modulepath=/puppetmodules
+ip addr add fd00:200::4/48 dev Pythagore-eth2
