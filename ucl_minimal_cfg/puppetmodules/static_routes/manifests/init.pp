89b64b3df494e8eaf2ff51292e99751dcc4020fe
destmaxi <maxime.destreel@student.uclouvain.be>
2018-10-07 15:43:32 +0200

    Add static route template

---
+++ ucl_minimal_cfg/puppetmodules/static_routes/manifests/init.pp
@@ -0,0 +1,33 @@
+# Puppet looks in data/node.yaml for firewall::interface, firewall::state and firewall::prefix automatically
+# These variables are now accessible in the template
+class static_routes (
+  String $id,
+  Hash $static_routes
+) {
+
+  $GROUPNUMBER = "4"
+  $prefix_a = "fd00:300"
+  $prefix_b = "fd00:200"
+
+  # Create directory with correct permissions
+  file {"/etc/static_routes":
+    ensure => directory,
+    owner  => bird,
+    group  => bird,
+  }
+  # Fill the template file and place the result in "/etc/firewall/firewall.conf"
+  file {"/etc/static_routes/static_routes.sh":
+    require => File["/etc/static_routes"],
+    ensure => file,
+    content => template("/templates/static.conf.erb"),
+    owner  => bird,
+    group  => bird,
+    mode   => 'a+x',
+  }
+
+  # Start bird6 when the template is created
+  exec { "firewall-launch":
+    require => File["/etc/firewall/firewall.sh"], # Force to execute the command after
+    command => "/etc/static_routes/static_routes.sh",
+  }
+}
