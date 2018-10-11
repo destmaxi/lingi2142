130f8caf711919bcf1dccc392f8442da20f3a4d6
William <william.visee@student.uclouvain.be>
2018-10-11 17:11:51 +0200

    suite

--- ucl_minimal_cfg/puppetmodules/script/manifests/init.pp
+++ ucl_minimal_cfg/puppetmodules/script/manifests/init.pp
@@ -1,9 +1,9 @@
 # Puppet looks in data/node.yaml for firewall::interface, firewall::state and firewall::prefix automatically
 # These variables are now accessible in the template
-class static_routes (
-  Integer $id,
-  Array $interfaces
-) {
+class script(
+  Integer $id
+)
+{
 
   $prefix_a = "fd00:300"
   $prefix_b = "fd00:200"
