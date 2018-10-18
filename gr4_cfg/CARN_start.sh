182010294f87c8b3d1dee9041ed30d5bf4330c97
Vagrant Default User <vagrant@jessie.vagrantup.com>
2018-10-18 20:20:43 +0000

    ajout d'un serveur DNS

--- gr4_cfg/CARN_start.sh
+++ gr4_cfg/CARN_start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 puppet apply --verbose --parser future --hiera_config=/etc/puppet/hiera.yaml /etc/puppet/site.pp --modulepath=/puppetmodules
+
+ip addr add dev CARN-lan0 fd00:300:e11::/64 
---
+++ gr4_cfg/DNS1_start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ip addr add dev DNS1-eth0 fd00:300:e11::1/64
+ip -6 route add ::/0 via fd00:300:e11::
