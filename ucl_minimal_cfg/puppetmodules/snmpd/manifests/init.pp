2356335dfa5d2502cf6abcc782fb7f94da8d2103
Vagrant Default User <vagrant@jessie.vagrantup.com>
2018-10-18 08:23:42 +0000

    fichiers conf generes

--- ucl_minimal_cfg/puppetmodules/snmpd/manifests/init.pp
+++ ucl_minimal_cfg/puppetmodules/snmpd/manifests/init.pp
@@ -1,32 +1,27 @@
 # Puppet looks in data/node.yaml for snmp automatically
 # These variables are now accessible in the template
-class snmpd (
-    String $agentaddress,
-    String $ro_community6,
-    String $rw_community6,
-    Hash $sys
-) {
+class snmpd () {
   # Get name of the node (lookup in data/node.yaml
   $location = lookup("name")
 
   # Create directory with correct permissions
   file {"/etc/snmp":
     ensure => directory,
-    owner  => snmp,
-    group  => snmp,
+    owner  => bird,
+    group  => bird,
   }
   # Fill the template file and place the result in "/etc/snmp/snmpd.conf"
   file {"/etc/snmp/snmpd.conf":
     require => File["/etc/snmp"],
     ensure => file,
     content => template("/templates/snmpd.conf.erb"),
-    owner  => snmp,
-    group  => snmp,
+    owner  => bird,
+    group  => bird,
   }
 
   # Start snmpd when the template is created
   exec { "snmpd-launch":
     require => File["/etc/snmp/snmpd.conf"], # Force to execute the command after
-    #command => "service snmpd start", # IS THAT REALLY CORRECT ???????
+    command => "service snmpd start", # IS THAT REALLY CORRECT ???????
   }
 }
