761339b38b65876627fd3865fb5ac927b4d3ac48
Vagrant Default User <vagrant@jessie.vagrantup.com>
2018-10-20 12:42:14 +0000

    reso conf

--- ucl_minimal_cfg/MONI_start.sh
+++ ucl_minimal_cfg/MONI_start.sh
@@ -3,3 +3,6 @@
 ip link set dev MONI-eth0 up
 ip addr add dev MONI-eth0 fd00:300:4:e10::2/64
 ip -6 route add ::/0 via fd00:300:4:e10::
+
+echo 'wazaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa------------------------------------------------------------------'
+sh ucl_minimal_cfg/MONI/check_test.sh >> ucl_minimal_cfg/MONI/logs.txt
