6d2115283458bee9204b513f29cd78747cfe3573
destmaxi <maxime.destreel@student.uclouvain.be>
2018-10-20 16:59:54 +0200

    Add student lan on mich

--- config_log.sh
+++ config_log.sh
@@ -1,7 +1,10 @@
-routers = "HALL PYTH MICH SH1C CARN STEV"
+#!/bin/bash
+
+routers="HALL PYTH MICH SH1C CARN STEV"
 
 for r in $routers; do
   mkdir -p gr4_cfg/r/log
-  sudo echo > gr4_cfg/r/bird_log
+  sudo echo > gr4_cfg/r/log/bird_log
+  sudo echo > gr4_cfg/r/log/nmap.result
   cp /etc/protocols gr4_cfg/r
 done
