116163e436f71155c0f519cea8bb8067b87496a5
destmaxi <maxime.destreel@student.uclouvain.be>
2018-10-20 18:33:05 +0200

    Fix - wrong dir

--- config_log.sh
+++ config_log.sh
@@ -6,5 +6,5 @@ for r in $routers; do
   mkdir -p gr4_cfg/r/log
   sudo echo > gr4_cfg/r/log/bird_log
   sudo echo > gr4_cfg/r/log/nmap.result
-  cp /etc/protocols gr4_cfg/r
+  cp /etc/protocols gr4_cfg/r/protocols
 done
