a194727f36e88797be55b3de5a34963acf82d000
Vagrant Default User <vagrant@jessie.vagrantup.com>
2018-10-22 14:56:05 +0000

    Fix - bug with config.sh

--- config_log.sh
+++ config_log.sh
@@ -3,8 +3,8 @@
 routers="HALL PYTH MICH SH1C CARN STEV"
 
 for r in $routers; do
-  mkdir -p gr4_cfg/r/log
-  sudo echo > gr4_cfg/r/log/bird_log
-  sudo echo > gr4_cfg/r/log/nmap.result
-  cp /etc/protocols gr4_cfg/r/protocols
+  mkdir -p gr4_cfg/$r/log
+  sudo echo > gr4_cfg/$r/log/bird_log
+  sudo echo > gr4_cfg/$r/log/nmap.result
+  cp /etc/protocols gr4_cfg/$r/protocols
 done
