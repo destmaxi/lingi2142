18d32163c59a88185d9704c235ad136166aa4c59
Vagrant Default User <vagrant@jessie.vagrantup.com>
2018-10-23 12:57:57 +0000

    dns work

--- create_network.sh
+++ create_network.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # List here all files from /etc that should be copied
-ETC_IMPORT=(hosts passwd group manpath.config services alternatives)
+ETC_IMPORT=(hosts passwd group manpath.config services alternatives init.d)
 ## See the bottom for the bootstrap
 
 # Execute a script from the node's config dir
--- gr4_cfg/CARN_start.sh
+++ gr4_cfg/CARN_start.sh
@@ -3,3 +3,5 @@
 puppet apply --verbose --parser future --hiera_config=/etc/puppet/hiera.yaml /etc/puppet/site.pp --modulepath=/puppetmodules
 
 ip addr add dev CARN-lan0 fd00:300:4:e11::/64 
+named
+radvd
