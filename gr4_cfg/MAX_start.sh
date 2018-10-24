59fc8bc768e7be1ea7155adc5324a3004e7b334b
destmaxi <maxime.destreel@student.uclouvain.be>
2018-10-24 16:06:11 +0200

    Change id

--- gr4_cfg/MAX_start.sh
+++ gr4_cfg/MAX_start.sh
@@ -1,4 +1,4 @@
-ip addr add dev MAX-eth0 fd00:300:4:0a30::1/64
-ip addr add dev MAX-eth0 fd00:200:4:0a30::1/64
+ip addr add dev MAX-eth0 fd00:300:4:0a30::2/64
+ip addr add dev MAX-eth0 fd00:200:4:0a30::2/64
 
 ip -6 route add ::/0 via fd00:300:4:0a30::
