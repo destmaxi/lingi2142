36d920ecc2dd1901206c45a581a5d97252af2b35
destmaxi <vagrant@jessie.vagrantup.com>
2018-10-26 15:56:05 +0000

    Add tunnel on pyth

--- gr4_cfg/PYTH_start.sh
+++ gr4_cfg/PYTH_start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 puppet apply --verbose --parser future --hiera_config=/etc/puppet/hiera.yaml /etc/puppet/site.pp --modulepath=/puppetmodules
+
+ip -6 tunnel add mytunnel mode ip6ip6 remote fd00:200:4:0f00::2
+ip -6 link set mytunnel up
+ip -6 addr add fd00:300:4:0f40::4 dev mytunnel
+ip -6 route add fd00:200:4::/48 dev mytunnel
