980ff3576998c4d7149a6f3b2846b75c4b8dbb21
Vagrant Default User <vagrant@jessie.vagrantup.com>
2018-10-28 13:48:19 +0000

    dns work

--- gr4_cfg/DNS2_start.sh
+++ gr4_cfg/DNS2_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ip addr add dev DNS2-eth0 fd00:300:4:e33::1/64
-ip -6 route add ::/0 via fd00:300:4:e33::
+ip addr add dev DNS2-eth0 fd00:300:4:e31::2/64
+ip -6 route add ::/0 via fd00:300:4:e31::
 named
