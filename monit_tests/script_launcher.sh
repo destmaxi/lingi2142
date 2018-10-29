f97b40cf06267de4aba5c2829d0f50b5f027595b
Vagrant Default User <vagrant@jessie.vagrantup.com>
2018-10-29 14:05:56 +0000

    frappe dns + redir script_laucnehr

--- monit_tests/script_launcher.sh
+++ monit_tests/script_launcher.sh
@@ -4,15 +4,15 @@ sleep 10 #add some time just to be sure about the network creation
 
 while true
 do
-    /home/vagrant/lingi2142/monit_tests/inter_routing_test.sh >> /home/vagrant/lingi2142/gr4_cfg/MONI/logs/ospf_logs.txt
+    /home/vagrant/lingi2142/monit_tests/inter_routing_test.sh 1>> /home/vagrant/lingi2142/gr4_cfg/MONI/logs/ospf_logs.txt 2>/dev/null
 
-    /home/vagrant/lingi2142/monit_tests/bgp_test.sh >> /home/vagrant/lingi2142/gr4_cfg/MONI/logs/bgp_logs.txt
+    /home/vagrant/lingi2142/monit_tests/bgp_test.sh 1>> /home/vagrant/lingi2142/gr4_cfg/MONI/logs/bgp_logs.txt 2>/dev/null
 
-    /home/vagrant/lingi2142/monit_tests/ping_bgp.sh >> /home/vagrant/lingi2142/gr4_cfg/MONI/logs/bgp_logs.txt
+    /home/vagrant/lingi2142/monit_tests/ping_bgp.sh 1>> /home/vagrant/lingi2142/gr4_cfg/MONI/logs/bgp_logs.txt 2>/dev/null
 
-    /home/vagrant/lingi2142/monit_tests/launch_snmp.sh >> /home/vagrant/lingi2142/gr4_cfg/MONI/logs/snmp_logs.txt
+    /home/vagrant/lingi2142/monit_tests/launch_snmp.sh 1>> /home/vagrant/lingi2142/gr4_cfg/MONI/logs/snmp_logs.txt2>/dev/null
 
-    /home/vagrant/lingi2142/monit_tests/dns_test.sh >> /home/vagrant/lingi2142/gr4_cfg/MONI/logs/dns_logs.txt
+    /home/vagrant/lingi2142/monit_tests/dns_test.sh 1>> /home/vagrant/lingi2142/gr4_cfg/MONI/logs/dns_logs.txt 2>/dev/null
 
     sleep 300 #Launching all the scripts every 5 min
 
