802d1aea7b67cf17d8143f8ebfe6430715e23283
destmaxi <maxime.destreel@student.uclouvain.be>
2018-10-29 14:11:06 +0100

    Fix: some dependencies are not installed

--- provision.sh
+++ provision.sh
@@ -13,13 +13,7 @@ apt-get -y -qq --force-yes update
 #fi
 #apt-get -y -qq --force-yes update
 
-apt-get -y -qq --force-yes install git bash vim-nox tcpdump nano\
-                                        bird6 quagga inotify-tools\
-                                    	iperf bind9 radvd\
-					snmp snmpd\
-					snmp-mibs-downloader\
-					python\
-					python-pip\
+apt-get -y -qq --force-yes install git bash vim-nox tcpdump nano bird6 quagga inotify-tools iperf bind9 radvd snmp snmpd snmp-mibs-downloader python python-pip
 
 pip install pysnmp
 # dependencies for puppet
