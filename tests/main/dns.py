13676a4f6f67a3abcc14086ae40b2aaccc387672
Vagrant Default User <roussieau.j@gmail.com>
2018-10-29 14:00:48 +0000

    fix dns test script

--- tests/main/dns.py
+++ tests/main/dns.py
@@ -8,9 +8,11 @@ import helpers
 
 class TestDDNS(unittest.TestCase):
 	def test_servail(self):
+		helpers.entete('Check if a dig return a servail error')
 		output = execute('../../monit_tests/dns_test.sh')
 		self.assertEquals(-1, output.find('SERVFAIL'))
 	def test_nxdomain(self):
+		helpers.entete('Check if a dig return a nxdomain error')
 		output = execute('../../monit_tests/dns_test.sh')
 		self.assertEquals(-1, output.find('NXDOMAIN'))
 
